cc02821f15c7ab2172b6e1630b4735508a8ada3d
小仔 <71455438+chris58530@users.noreply.github.com>
2022-09-02 22:35:56 +0800

    新增2D移動

---
+++ Unity commonl used code project/Assets/Scripts/MoveControl2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl2D : MonoBehaviour
+{
+    [SerializeField] float moveInput;
+    [SerializeField] float moveSpeed;
+    Rigidbody2D myRB;
+
+    void Awake()
+    {
+        myRB = GetComponent<Rigidbody2D>();    
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move_Jump();
+    }
+    void Move_Jump()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        myRB.velocity = new Vector2(moveInput * moveSpeed, myRB.velocity.y);
+    }
+}
